ddd3f3cc6a913828e6a24b16764753a0567c9f03
Jan K. Marucha <jkmarucha@gmail.com>
2026-06-03 23:49:38 +0200

    eslint rules tweak

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,4 +7,9 @@ export default defineConfig([
   { ignores: ["dist/"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
 ]);
